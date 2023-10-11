a0de567c81f4ee4c1dec4568bd9b0f2aae985259
Donnelly Earley <90118182+DonnellyEarley@users.noreply.github.com>
2023-10-11 18:29:21 -0500

    fixed dark mode on multiple pages issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const switchElement = document.getElementById("mode_switch");
+document.addEventListener("turbo:load", function () {
   const htmlTag = document.documentElement;
+  const switchElement = document.getElementById("mode_switch");
+  const modeSwitchLabel = document.getElementById("mode_switch_label");
 
   let isOn = false;
 
@@ -8,9 +9,11 @@ document.addEventListener("DOMContentLoaded", function () {
     isOn = !isOn;
     if (isOn) {
       htmlTag.setAttribute("data-bs-theme", "dark");
+      modeSwitchLabel.innerHTML = '<i class="bi bi-moon-fill"></i>';
     } else {
       htmlTag.setAttribute("data-bs-theme", "light");
+      modeSwitchLabel.innerHTML = '<i class="bi bi-brightness-high-fill"></i>';
     }
   });
 });
-debugger;
+// debugger;
