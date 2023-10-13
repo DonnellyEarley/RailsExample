17bd658ed0916233da52364484f6fc586220bb36
Donnelly Earley <donnellyearley@gmail.com>
2023-10-13 10:17:12 -0500

    added ngrok support

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # config to ngrok for temporary hosting
+    config.hosts << "mammal-adapting-formally.ngrok-free.app"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
